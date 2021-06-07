15e7c10427a1c048eefff5a8bdb433ffd19c6021
Jarno Rajahalme <jarno@isovalent.com>
2021-06-07 13:17:10 -0700

    clustermesh: Add config option to vm install
    Add config option to external workload (aka vm) install. '--config'
    can be passed multiple times with 'key=value' syntax, e.g.:
    
    $ cilium clustermesh vm install --config debug --config debug-verbose=flow
    
    Note that '--config debug' works without a value as it is a boolean
    option value of which Cilium agent assumes to be 'true' if missing.
    
    Add the '--config debug' option to external workload CI install script.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- .github/in-cluster-test-scripts/external-workloads-install.sh
+++ .github/in-cluster-test-scripts/external-workloads-install.sh
@@ -11,6 +11,6 @@ cilium clustermesh status --wait --wait-duration 5m
 cilium clustermesh vm create "${VM_NAME}" -n default --ipv4-alloc-cidr 10.192.1.0/30
 cilium clustermesh vm status
 
-cilium clustermesh vm install install-external-workload.sh
+cilium clustermesh vm install install-external-workload.sh --config debug
 
 kubectl -n kube-system create cm install-external-workload-script --from-file=script=install-external-workload.sh
